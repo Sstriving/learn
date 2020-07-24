0e14fa0e6bec25238fbc2392a7d96ff401974741
liuxukang <liuxukang@megvii.com>
2020-07-24 19:25:17 +0800

    树的遍历

--- 数据结构/树结构/index.js
+++ 数据结构/树结构/index.js
@@ -1,7 +1,5 @@
 // js写一个树结构
 
-
-
 /****
  * 概念 ： 
  * 1.节点 ： 树结构中的每一个子项、包含数据项和指向其他节点的指针
@@ -23,55 +21,173 @@
  */
 // 二叉树的类定义
 
-const BinTreeNode = function(data) {
-    this.data = data
-    this.leftChild = null; // 左孩子
-    this.rightChild = null; // 右孩子
-    this.parentNode = null; // 父节点
-    // data表示数据，其他都是指针
-}
+const BinTreeNode = function (data) {
+  this.data = data;
+  this.leftChild = null; // 左孩子
+  this.rightChild = null; // 右孩子
+  this.parentNode = null; // 父节点
+  // data表示数据，其他都是指针
+};
 
 function BinaryTree() {
-    const root = null; // 根节点
-    
-    // 采用广义表表示 建立二叉树方法
-    this.init_tree = function(string) {
+  const root = null; // 根节点
 
-    }
-
-    // 使用的都是深度优先遍历
-    // 中序遍历
-    this.in_order = function (node) {
-        if(node === null) {
-            return
+  // 采用广义表表示 建立二叉树方法
+  this.init_tree = function (string) {
+    var stack = new Stack.Stack();
+    var k = 0;
+    var new_node = null;
+    for (var i = 0; i < string.length; i++) {
+      var item = string[i];
+      if (item == "(") {
+        stack.push(new_node);
+        k = 1;
+      } else if (item == ")") {
+        stack.pop();
+      } else if (item == ",") {
+        k = 2;
+      } else {
+        new_node = BinTreeNode(item);
+        if (root == null) {
+          root = new_node;
+        } else if (k == 1) {
+          // 左子树
+          var top_item = stack.top();
+          top_item.leftChild = new_node;
+          new_node.parentNode = top_item;
+        } else {
+          // 右子树
+          var top_item = stack.top();
+          top_item.rightChild = new_n;
+          ode;
+          new_node.parentNode = top_item;
         }
-        this.in_order(node.leftChild);
-        // do something
-        console.log(node.data);
-        this.in_order(node.rightChild);
+      }
     }
+  };
 
-    // 前序遍历
-    this.pre_order = function (node) {
-        if(node === null) {
-            return
-        }
-        // do something
-        console.log(node.data);
-        this.pre_order(node.leftChild)
-        this.pre_order(node.rightChild)
+  // 使用的都是深度优先遍历
+  // 中序遍历
+  this.in_order = function (node) {
+    if (node === null) {
+      return;
     }
+    this.in_order(node.leftChild);
+    // do something
+    console.log(node.data);
+    this.in_order(node.rightChild);
+  };
 
-    // 后序遍历 先访问子节点，最后访问根节点
-    this.post_order = function (node ) {
-        if(node === null) {
-            return
-        }
-        this.post_order(node.leftChild)
-        this.post_order(node.rightChild)
-        // do something
-        console.log(node.data)
+  // 前序遍历
+  this.pre_order = function (node) {
+    if (node === null) {
+      return;
     }
+    // do something
+    console.log(node.data);
+    this.pre_order(node.leftChild);
+    this.pre_order(node.rightChild);
+  };
 
+  // 后序遍历 先访问子节点，最后访问根节点
+  this.post_order = function (node) {
+    if (node === null) {
+      return;
+    }
+    this.post_order(node.leftChild);
+    this.post_order(node.rightChild);
+    // do something
+    console.log(node.data);
+  };
+
+  // 广度优先遍历
+  this.layer_traver = function (node) {
+    let head = 0;
+    let tail = 0;
+    const arr = [];
+    let temp;
+    if (node !== null) {
+      arr[head] = node;
+      tail++;
+    } else {
+      return;
+    }
+    while (head < tail) {
+      temp = arr[head];
+      // do something
+      console.log(temp.data);
+      // 若左侧子树非空则会读取其子树
+      if (temp.leftChild !== null) {
+        arr[tail] = temp.leftChild;
+        tail++;
+      }
+      // 同上
+      if (temp.rightChild !== null) {
+        arr[tail] = temp.rightChild;
+        tail++;
+      }
+      head++;
+    }
+    return;
+  };
+
+  // 返回节点数量
+  this.tree_node_count = function (node) {
+    if (!node) {
+      return 0;
+    }
+    const left_node_count = this.tree_node_count(node.leftChild);
+    const right_node_count = this.tree_node_count(node.rightChild);
+    return left_node_count + right_node_count + 1;
+  };
+
+  // 返回树的高度
+  this.tree_height = function (node) {
+    if (!node) {
+      return 0;
+    }
+    const left_child_height = this.tree_height(node.leftChild);
+    const right_child_height = this.tree_height(node.rightChild);
+    if (left_child_height > right_child_height) {
+      return left_child_height + 1;
+    } else {
+      return right_child_height + 1;
+    }
+  };
+
+    //   查找节点
+  this.find_node = function(node,data) {
+    if(!node) {
+        return null
+    }
+    if(node.data === data) {
+        return node
+    }
+    const left_res = this.find_node(node.leftChild,data)
+    if(left_res){
+        return left_res
+    }
+    return this.find_node(node.rightChild,data)
+
+
+
+  }
 }
 
+// 中序遍历  迭代的写法
+var inorderTraversal = function(root) {
+  const arr = []
+  const stack = []
+  let curr = root
+
+  while(curr !== null || stack.length > 0) {
+    while(curr !== null) {
+      stack.push(curr)
+      curr = curr.left
+    }
+    curr = stack.pop()
+    arr.push(curr.val)
+    curr = curr.right    
+  }
+  return arr;
+}
